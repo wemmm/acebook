dca295cebc6ba06807d8eeeb758be62dd3b25d9a
wemmm <jenny.wem@gmail.com>
2017-06-28 13:41:14 +0100

    Posts can now have line breaks

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -4,3 +4,11 @@ def create_new_post
   fill_in "Message", with: "Test post!"
   click_button "Submit"
 end
+
+def create_long_post
+  visit "/posts"
+  click_link "New post"
+  fill_in "Message", with: "Hello world,\n How are you?"
+  click_button "Submit"
+  expect(page).to have_content("Hello world,\n How are you?")
+end
