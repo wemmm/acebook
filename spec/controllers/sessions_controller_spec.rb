28e82b36bb22271e98f54b63b4dd442cce663c18
Kynosaur <kye.bracey@gmail.com>
2017-06-28 10:18:37 +0100

    Adds sessions routes, sessions controller and view with login form

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "Can get new login" do
+      get login_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
