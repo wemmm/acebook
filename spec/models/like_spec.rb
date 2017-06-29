7608478783458669a12e1bd4cc5fbe1bbc82f67e
Kynosaur <kye.bracey@gmail.com>
2017-06-29 19:40:27 +0100

    Adds missing unit tests for like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'can create a new like' do
+    user = User.create(username: 'Jade', password: '123456', email: 'jade@jade.com')
+    post = user.posts.create(message: "Hello test!")
+    like = post.likes.build
+    like.user = user
+    like.save
+    expect(like).to be_valid
+  end
+
+  it 'will not create a like without a user' do
+    user = User.create(username: 'Jade', password: '123456', email: 'jade@jade.com')
+    post = user.posts.create(message: "Hello test!")
+    like = post.likes.create
+    expect(like).not_to be_valid
+  end
+
+  it 'will not create a like without a post' do
+    like = Like.create
+    expect(like).not_to be_valid
+  end
 end
