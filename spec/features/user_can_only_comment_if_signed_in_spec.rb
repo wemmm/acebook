d70792831c1d90fa1234dbdc7b27101ff588a24b
wemmm <jenny.wem@gmail.com>
2017-06-29 11:10:24 +0100

    Adds test for users being unable to comment if they are not signed in

---
+++ spec/features/user_can_only_comment_if_signed_in_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature 'Comment restriction', type: :feature do
+  scenario 'Can only comment if signed in' do
+    sign_up
+    create_new_post
+    click_link('Log Out')
+    visit('/posts')
+    click_link('Comments')
+    expect(page).to have_content('Please sign up to comment on a post!')
+    expect(page).not_to have_field('comment_body')
+    expect(page).not_to have_button('Post comment')
+    sign_up
+    visit '/posts/1'
+    expect(page).to have_field('comment_body')
+    expect(page).to have_button('Post comment')
+  end
+end
