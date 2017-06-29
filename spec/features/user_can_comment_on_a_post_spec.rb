3dd7450c9c6b8447f0c2cc92989b909d422244b7
Kynosaur <kye.bracey@gmail.com>
2017-06-29 15:41:32 +0100

    Adds test for comments belonging to a user

--- spec/features/user_can_comment_on_a_post_spec.rb
+++ spec/features/user_can_comment_on_a_post_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature 'Comments', type: :feature do
-  scenario 'Can comment on a post' do
+  scenario 'User can comment on a post' do
     sign_up
     create_new_post
     click_on('Comments')
     fill_in('comment_body', with: 'Small woodland creature')
     click_button('Post comment')
     expect(page).to have_content('Small woodland creature')
+  end
+
+  scenario "Belong to a user and display the commenter's username in the view" do
+    sign_up
+    create_new_post
+    click_on('Comments')
+    fill_in('comment_body', with: 'Small woodland creature')
+    click_button('Post comment')
+    expect(page).to have_content('Comment by Jade')
   end
 end
