416ecc3e88d3a2c59761655750b85043490760bc
Kynosaur <kye.bracey@gmail.com>
2017-06-29 14:55:42 +0100

    Updates posts model - posts now belong to users.  Updates posts view to show poster with link to appropriate users/show route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628142148) do
+ActiveRecord::Schema.define(version: 20170629133300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20170628142148) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +48,5 @@ ActiveRecord::Schema.define(version: 20170628142148) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "likes", "posts"
+  add_foreign_key "posts", "users"
 end
